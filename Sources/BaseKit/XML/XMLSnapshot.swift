6ac285c5afe78341d3d307037dc7c3a9f18f71ae
Andrew Finnell <andy@losingfight.com>
2024-10-05 15:49:30 -0400

    Allow XMLSnapshot to build an entire doc (but not insert)

--- Sources/BaseKit/XML/XMLSnapshot.swift
+++ Sources/BaseKit/XML/XMLSnapshot.swift
@@ -9,3 +9,33 @@ public struct XMLSnapshot: Sendable {
         self.values = values
     }
 }
+
+public extension XMLSnapshot {
+    init<Content: XML>(@XMLSnapshotBuilder builder: () -> Content) {
+        let built = builder()
+        var storage = [XMLID: XMLValue]()
+        let context = XMLBuilderContext(
+            indent: 0,
+            variables: [:]
+        )
+        var allRootValues = [XMLValue]()
+        
+        let prefixString = "\(context.indentString)"
+        let prefixText = XMLText(id: XMLID(), parentID: nil, characters: prefixString)
+        storage[prefixText.id] = .text(prefixText)
+        allRootValues.append(.text(prefixText))
+
+        var refIDs = [XMLID: XMLReferenceIDFuture]()
+        allRootValues.append(
+            contentsOf: built.values(
+                for: nil,
+                context: context,
+                storingInto: &storage,
+                registeringReferenceInto: &refIDs
+            )
+        )
+        
+        roots = allRootValues.map(\.id)
+        values = storage
+    }
+}
