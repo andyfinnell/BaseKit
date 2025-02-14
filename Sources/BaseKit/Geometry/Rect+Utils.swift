85975e2c01caa7015a82cee192ae13a1a480e90a
Andrew Finnell <andy@losingfight.com>
2025-02-14 14:09:01 -0500

    Anchors

--- Sources/BaseKit/Geometry/Rect+Utils.swift
+++ Sources/BaseKit/Geometry/Rect+Utils.swift
@@ -1,22 +1,30 @@
 import Foundation
 
 public extension Rect {
-    var topLeft: Point {
-        Point(x: minX, y: minY)
-    }
+    var topLeft: Point { Point(x: minX, y: minY) }
+    var top: Point { Point(x: midX, y: minY) }
+    var topRight: Point { Point(x: maxX, y: minY) }
+    var middleLeft: Point { Point(x: minX, y: midY) }
+    var middle: Point { Point(x: midX, y: midY) }
+    var middleRight: Point { Point(x: maxX, y: midY) }
+    var bottomLeft: Point { Point(x: minX, y: maxY) }
+    var bottom: Point { Point(x: midX, y: maxY) }
+    var bottomRight: Point { Point(x: maxX, y: maxY) }
 
-    var topRight: Point {
-         Point(x: maxX, y: minY)
+    func anchor(for anchorPoint: AnchorPoint) -> Point {
+        switch anchorPoint {
+        case .topLeft: topLeft
+        case .topCenter: top
+        case .topRight: topRight
+        case .centerLeft: middleLeft
+        case .center: middle
+        case .centerRight: middleRight
+        case .bottomLeft: bottomLeft
+        case .bottomCenter: bottom
+        case .bottomRight: bottomRight
+        }
     }
-
-    var bottomLeft: Point {
-        Point(x: minX, y: maxY)
-    }
-
-    var bottomRight: Point {
-         Point(x: maxX, y: maxY)
-    }
-
+    
     func mightOverlap(_ bounds2: Rect, threshold: Real) -> Bool {
         let left = max(minX, bounds2.minX)
         let right = min(maxX, bounds2.maxX)
