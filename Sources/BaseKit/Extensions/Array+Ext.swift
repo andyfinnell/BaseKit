c7d76ff2a7d7de8282f4517d47310c02c588f5db
Andrew Finnell <andy@losingfight.com>
2025-02-22 11:31:16 -0500

    More Array extensions

--- Sources/BaseKit/Extensions/Array+Ext.swift
+++ Sources/BaseKit/Extensions/Array+Ext.swift
@@ -116,3 +116,19 @@ public extension Array {
         }
     }
 }
+
+public typealias IdentifiableArray<Element> = Array<Element> where Element: Identifiable
+
+public extension IdentifiableArray {
+    subscript(byID id: Element.ID) -> Element? {
+        get {
+            first(where: { $0.id == id })
+        }
+        set {
+            guard let i = firstIndex(where: { $0.id == id }), let newValue else {
+                return
+            }
+            self[i] = newValue
+        }
+    }
+}
