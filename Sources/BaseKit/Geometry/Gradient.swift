cb04cee1f020078e23024df5484fd1218245f44a
Andy Finnell <andy@losingfight.com>
2026-03-03 16:59:58 -0500

    expand gradient

--- Sources/BaseKit/Geometry/Gradient.swift
+++ Sources/BaseKit/Geometry/Gradient.swift
@@ -3,11 +3,17 @@ public struct Gradient: Hashable, Codable, Sendable {
         case linear
         case radial
     }
-    
+
+    public enum SpreadMethod: String, Hashable, Codable, Sendable {
+        case pad
+        case reflect
+        case `repeat`
+    }
+
     public struct Stop: Hashable, Codable, Sendable {
         public let offset: Double
         public let color: Color
-        
+
         public init(offset: Double, color: Color) {
             self.offset = offset
             self.color = color
@@ -19,12 +25,27 @@ public struct Gradient: Hashable, Codable, Sendable {
     public let end: Point
     public let stops: [Stop]
     public let boundingBox: Rect?
-    
-    public init(kind: Kind, start: Point, end: Point, stops: [Stop], boundingBox: Rect?) {
+    public let spreadMethod: SpreadMethod
+    public let gradientTransform: Transform?
+    public let focalPoint: Point?
+
+    public init(
+        kind: Kind,
+        start: Point,
+        end: Point,
+        stops: [Stop],
+        boundingBox: Rect?,
+        spreadMethod: SpreadMethod = .pad,
+        gradientTransform: Transform? = nil,
+        focalPoint: Point? = nil
+    ) {
         self.kind = kind
         self.start = start
         self.end = end
         self.stops = stops
         self.boundingBox = boundingBox
+        self.spreadMethod = spreadMethod
+        self.gradientTransform = gradientTransform
+        self.focalPoint = focalPoint
     }
 }
