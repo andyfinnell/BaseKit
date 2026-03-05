eae93fecead91162fc08e4406a3615c2eae8efe2
Andy Finnell <andy@losingfight.com>
2026-03-05 13:32:55 -0500

    Extending pattern support

--- Sources/BaseKit/Geometry/Pattern.swift
+++ Sources/BaseKit/Geometry/Pattern.swift
@@ -3,9 +3,24 @@ import Foundation
 public struct Pattern: Codable, Hashable, Sendable {
     public let name: String
     public let imageData: Data // TODO: how to make this efficient in rendering? cache?
-    
-    public init(name: String, imageData: Data) {
+    public let tileWidth: Double
+    public let tileHeight: Double
+    public let patternTransform: Transform?
+    public let boundingBox: Rect?
+
+    public init(
+        name: String,
+        imageData: Data,
+        tileWidth: Double = 0,
+        tileHeight: Double = 0,
+        patternTransform: Transform? = nil,
+        boundingBox: Rect? = nil
+    ) {
         self.name = name
         self.imageData = imageData
+        self.tileWidth = tileWidth
+        self.tileHeight = tileHeight
+        self.patternTransform = patternTransform
+        self.boundingBox = boundingBox
     }
 }
