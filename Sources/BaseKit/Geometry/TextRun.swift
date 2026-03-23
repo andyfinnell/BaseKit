eee4657ed0c6abee8677e801fc4e6cb6af405a71
Andy Finnell <andy@losingfight.com>
2026-03-22 21:12:33 -0400

    enhancing text runs

--- Sources/BaseKit/Geometry/TextRun.swift
+++ Sources/BaseKit/Geometry/TextRun.swift
@@ -9,9 +9,30 @@ public struct TextRun: Hashable, Codable, Sendable {
 
     public let text: String
     public let attributes: [Attribute]
-    
-    public init(text: String, attributes: [Attribute]) {
+    public let decorations: [Decoration]?
+    public let textDecorationLines: TextDecorationLine?
+    public let dx: Double
+    public let dy: Double
+
+    public init(
+        text: String,
+        attributes: [Attribute],
+        decorations: [Decoration]? = nil,
+        textDecorationLines: TextDecorationLine? = nil,
+        dx: Double = 0,
+        dy: Double = 0
+    ) {
         self.text = text
         self.attributes = attributes
+        self.decorations = decorations
+        self.textDecorationLines = textDecorationLines
+        self.dx = dx
+        self.dy = dy
+    }
+}
+
+extension TextRun {
+    public var needsPerRunRendering: Bool {
+        decorations != nil || textDecorationLines != nil || dx != 0 || dy != 0
     }
 }
